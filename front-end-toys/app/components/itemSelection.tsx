128ebf41eaba2198d267a7edbe9c7a760201c0f5
James Moore <jameswmoore99@gmail.com>
2024-06-25 09:53:06 -0600

    ~ Extrapolated item selection component

---
+++ front-end-toys/app/components/itemSelection.tsx
@@ -0,0 +1,33 @@
+const ItemSelection = () => {
+  return (
+    <div className="flex flex-wrap p-2 place-content-start w-10/12">
+      {/* Will be mapping over the amount of items later */}
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+      <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
+        <span className="text-xl">Item X</span>
+        <span className="text-l">Desc</span>
+      </div>
+    </div>
+  );
+};
+
+export default ItemSelection;
--- front-end-toys/app/page.tsx
+++ front-end-toys/app/page.tsx
@@ -1,36 +1,13 @@
+import ItemSelection from "./components/itemSelection";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-800">
       <div className="flex flex-col w-full items-center">
         <span className="text-2xl">Welcome to my Front End Toy Problems</span>
         <span>Click from the list below to view</span>
-        <div className="flex flex-wrap p-2 place-content-evenly w-10/12">
-          {/* Will be mapping over the amount of items later */}
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-          <div className="flex flex-col items-center justify-center m-2 w-36 h-40 bg-slate-700">
-            <span className="text-xl">Item X</span>
-            <span className="text-l">Desc</span>
-          </div>
-        </div>
+        {/* thought is to switch out different components, but item selection is the inital view */}
+        <ItemSelection />
       </div>
     </main>
   );
